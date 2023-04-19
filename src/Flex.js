034b4428fce64123d948ef028002487aa45946f0
gvishal14 <vishalgupta140701@gmail.com>
2023-04-19 15:56:54 +0530

    Created FlexBox

---
+++ src/Flex.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const Flex = () => {
+    return (
+        <View
+            style={[
+                styles.container,
+                {
+                    // Try setting `flexDirection` to `"row"`.
+                    backgroundColor: 'yellow'
+
+                },
+            ]}>
+            <View style={{ flexDirection: 'row', flex: 1, backgroundColor: 'yellow', }} >
+                <View style={{ flex: 1, backgroundColor: 'red', margin: 5 }} ></View>
+                <View style={{ flex: 1, backgroundColor: 'red', margin: 5 }} ></View>
+                <View style={{ flex: 1, backgroundColor: 'red', margin: 5 }} ></View>
+                <View style={{ flex: 1, backgroundColor: 'red', margin: 5 }} ></View>
+            </View>
+            <View style={{  flexDirection: 'row',flex: 1, backgroundColor: 'yellow', }} >
+                <View style={{ flex: 2, backgroundColor: 'red', margin: 5 }} ></View>
+                <View style={{ flex: 1, backgroundColor: 'yellow', }} ></View>
+                <View style={{ flex: 1, backgroundColor: 'red', margin: 5 }} ></View>
+                
+            </View>
+            <View style={{ flexDirection: 'row', flex: 1, backgroundColor: 'yellow', }} >
+            <View style={{ flex: 1, backgroundColor: 'yellow', margin: 5 }} ></View>
+                <View style={{ flex: 2, backgroundColor: 'red', margin: 5 }} ></View>
+                <View style={{ flex: 1, backgroundColor: 'red', margin: 5 }} ></View>
+            
+            </View>
+
+
+            <View style={{ flex: 3, backgroundColor: 'red', margin: 5, }} ></View>
+            <View style={{ flex: 2, backgroundColor: 'red', margin: 5, }} ></View>
+            <View style={{ flex: 1.5, backgroundColor: 'red', margin: 5, }} ></View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+
+    },
+});
+
+export default Flex;
